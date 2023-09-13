b90225024bbd25947297f149b135a0c1035b439b
Артём Долбик <aptdoll@yandex.ru>
2023-09-13 17:30:23 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,28 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start():
-    return """
-<!doctype html>
-<html>
-    <head>
-        <title>НГТУ, ФБ, Лабораторные работы</title>
-    </head>
-    <body>
-        <header>
-            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
-        </header>
-
-        <h1><a href="/lab1" target="blank">Лабораторная работа 1</a></h1>
-    
-        <footer>
-            &copy; Долбик Артём, ФБИ-13, 3 курс, 2023
-        </footer>
-    </body>
-</html>
-""" 
+    return redirect ("/menu", code=302)
 
 @app.route("/lab1")
 def lab1():
@@ -42,6 +24,28 @@ def lab1():
         <br />использующий набор инструментов Werkzeug, а также шаблонизатор jinja2. 
         <br />Относится к категории так называемых микрофреймворков - минималистичных каркасов 
         <br />веб-приложений, сознательно предоставляющих лишь самые базовые возможности</p>
+        <footer>
+            &copy; Долбик Артём, ФБИ-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+""" 
+
+@app.route("/menu")
+def menu():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <h1><a href="/lab1" target="blank">Лабораторная работа 1</a></h1>
+    
         <footer>
             &copy; Долбик Артём, ФБИ-13, 3 курс, 2023
         </footer>
