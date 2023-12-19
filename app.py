e26501308cf43a96953f8d78bbb6ae2432f18b0d
Артём Долбик <aptdoll@yandex.ru>
2023-12-19 19:10:57 +0700

    Роуты

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
+
+
 @app.route("/")
 @app.route("/index")
 def start():
     return redirect("/menu", code=302)
-     
+    
 
 @app.route("/lab1")
 def lab1():
@@ -14,7 +16,10 @@ def lab1():
 <!doctype html>
 <html>
     <head>
-        <title>Долбик Артём Евгеньевич, лабораторная1</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Долбик Артём Евгеньевич, лабораторная 1</title>
+        <link rel="stylesheet" type="text/css" href="/static/lab1.css">
     </head>
     <body>
         <header>
@@ -27,6 +32,15 @@ def lab1():
         <br />Относится к категории так называемых микрофреймворков - минималистичных каркасов
         <br />веб-приложений, сознательно предоставляющих лишь самые базовые возможности</p>
     
+        <p><a href="/menu" target="blank">Меню</a></p>
+
+    <h2>Реализованные роуты</h2>
+        <p><a href="/lab1/oak" target="blank">Дуб</a></p>
+        <p><a href="/lab1/student" target="blank">Студент</a></p>
+        <p><a href="/lab1/python" target="blank">Питон</a></p>
+        <p><a href="/lab1/helicopter" target="blank">Вертолёт</a></p>
+        
+
         <footer>
             &copy; Долбик Артём, ФБИ-13, 3 курс, 2023
         </footer>
@@ -41,7 +55,10 @@ def menu():
 <!doctype html>
     <html>
         <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>НГТУ, ФБ, Лабораторные работы</title>
+            <link rel="stylesheet" type="text/css" href="/static/lab1.css">    
         </head>
     <body>
         <header>
@@ -61,10 +78,62 @@ def menu():
 def oak():
     return '''
 <!doctype html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="/static/lab1.css">
+    </head>
     <html>
     <body>
         <h1>Дуб</h1>
         <img src="'''+ url_for('static', filename='oak.jpg') + '''">
+    </body>
+</html>
+'''
+
+@app.route("/lab1/student")
+def student():
+    return '''
+<!doctype html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="/static/lab1.css">
+    </head>
+    <html>
+    <body>
+        <h1>Долбик Артём Евгеньевич</h1>
+        <img src="'''+ url_for('static', filename='NGTU.jpg') + '''">
+    </body>
+</html>
+'''
+
+@app.route("/lab1/python")
+def python():
+    return '''
+<!doctype html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="/static/lab1.css">
+    </head>
+    <html>
+    <body>
+        <p>Высокоуровневый язык программирования общего назначения с динамической строгой типизацией и автоматическим управлением памятью, 
+        <br />ориентированный на повышение производительности разработчика, читаемости кода и его качества, 
+        <br />а также на обеспечение переносимости написанных на нём программ.</p>
+        <img src="'''+ url_for('static', filename='python.jpg') + '''">
+    </body>
+</html>
+'''
+
+@app.route("/lab1/helicopter")
+def helicopter():
+    return '''
+<!doctype html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="/static/lab1.css">
+    </head>
+    <html>
+    <body>
+        <p>Вертолёт — винтокрылый летательный аппарат вертикального взлёта и посадки, 
+        <br />у которого подъёмная и движущая силы на всех этапах полёта создаются одним или несколькими несущими винтами 
+        <br/>с приводом от одного или нескольких двигателей.</p>
+        <img src="'''+ url_for('static', filename='helicopter.jpg') + '''">
     </body>
 </html>
 '''
