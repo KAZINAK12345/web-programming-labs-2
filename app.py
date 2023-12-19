5610f645ec2441c1869cc822084edfb97092793d
Артём Долбик <aptdoll@yandex.ru>
2023-12-19 18:01:46 +0700

    Меню

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
-    return """
+    return"""
+     
 <!doctype html>
 <html>
     <head>
@@ -22,3 +24,30 @@ def start():
         </body>
     </html>
 """
+
+@app.route("/lab1")
+def lab1():
+    return'''
+
+<!doctype html>
+<html>
+    <head>
+        <title>Долбик Артём Евгеньевич, лабораторная1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+    <h1>web-сервер на flask</h1>
+        <p>Flask - фреймворк для создания веб-приложений на языке программирования Python,
+        <br />использующий набор инструментов Werkzeug, а также шаблонизатор jinja2.
+        <br />Относится к категории так называемых микрофреймворков - минималистичных каркасов
+        <br />веб-приложений, сознательно предоставляющих лишь самые базовые возможности</p>
+    
+        <footer>
+            &copy; Долбик Артём, ФБИ-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+'''
